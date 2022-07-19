cd45fc2a76138e0bd6e29d5f5dcc6cf4b54b711f
KristinaVaskovich <kristina.vaskovich@bk.ru>
2022-07-19 10:06:52 +0300

    store class changes

--- store/src/main/java/Store.java
+++ store/src/main/java/Store.java
@@ -1,7 +1,12 @@
 import categories.Category;
 
+import java.util.ArrayList;
+
 public class Store {
-    private Category fillList(int numberOfItems){
-        return RandomStorePopulator.fillListOfProducts(numberOfItems);
+    private ArrayList<Category> categoryArrayList = new ArrayList<>();
+
+    private ArrayList<Category> fillList(int numberOfItems) {
+        categoryArrayList.add(RandomStorePopulator.fillListOfProducts(numberOfItems));
+        return categoryArrayList;
     }
 }
